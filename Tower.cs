0e2a15d574c9b46bcd267c39262259c431d52b3a
batel <batelik.55@gmail.com>
2023-05-10 11:40:11 +0300

    Add project files.

---
+++ Tower.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterTower {
+	internal class Tower 
+	{
+		protected double Height { get; set; }
+		protected double Width { get; private set; }
+
+		private const int MIN_HEIGHT = 2;
+		private const int MIN_WIDTH = 0;
+
+		protected Tower() 
+		{
+			Initialize();
+		}
+
+		private void Initialize() 
+		{
+			double input;
+			Console.Write("Enter tower height: ");
+			input = double.Parse(Console.ReadLine());
+			if (input >= MIN_HEIGHT) {
+				Height = input;
+				Console.Write("Enter tower width: ");
+				input = double.Parse(Console.ReadLine());
+				if (input > MIN_WIDTH) {
+					Width = input;
+				}
+				else
+					Console.WriteLine("Invalid value. Tower width must be greater than {0}", MIN_WIDTH);
+			}
+			else
+				Console.WriteLine("Invalid value. Tower width must be greater than or equal to {0}", MIN_HEIGHT);
+		}
+		
+	}
+}
